bd43b77bc7dbcadfea3dcc0f5650a1e84c7f3ae9
João Pedro <pedro.as2000@gmail.com>
2025-03-21 17:51:22 -0300

    test margin

--- frontend/pages/registro.js
+++ frontend/pages/registro.js
@@ -97,7 +97,7 @@ export default function Registro() {
           </section>
 
           <section id="top" className="d-flex flex-column min-vh-100">
-            <div className="container row m-auto" style={{marginBottom: "100px", marginTop: "100px"}}>
+            <div className="container row m-auto mb-lg-5 mt-lg-5">
               <div className="col-md-6 bg-light border m-auto p-4 rounded">
                 <h2 className="text-center">Registro</h2>
                 <form onSubmit={handleSubmit}>
