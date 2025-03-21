7baacadbfae9d7984e0e27dbbf8e95e2b46c87aa
João Pedro <pedro.as2000@gmail.com>
2025-03-21 20:54:31 -0300

    update listar-lojas

---
+++ frontend/pages/api/listar-lojas.js
@@ -0,0 +1,30 @@
+// pages/api/listar-lojas.js
+export default async function handler(req, res) {
+    if (req.method === "GET") {
+      try {
+        // Fazer a requisição para o Back4App para listar as lojas
+        const response = await fetch("https://parseapi.back4app.com/classes/Loja", {
+          method: "GET",
+          headers: {
+            "X-Parse-Application-Id": process.env.BACK4APP_APP_ID,
+            "X-Parse-JavaScript-Key": process.env.BACK4APP_JS_KEY,
+            "Content-Type": "application/json",
+          },
+        });
+  
+        const data = await response.json();
+  
+        if (response.ok) {
+          // Retornar a lista de lojas
+          res.status(200).json({ lojas: data.results });
+        } else {
+          res.status(400).json({ message: "Erro ao carregar lojas." });
+        }
+      } catch (error) {
+        console.error("Erro ao conectar com o Back4App:", error);
+        res.status(500).json({ message: "Erro ao conectar com o servidor." });
+      }
+    } else {
+      res.status(405).json({ message: "Método não permitido." });
+    }
+  }
